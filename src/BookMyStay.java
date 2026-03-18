4953751d7efe430c67ed97470ce147048f818c22
sk-1698 <sk1698@srmist.edu.in>
2026-03-18 14:57:39 +0530

    UC2 - Basic room types and static availability

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,12 +1,65 @@
 public class BookMyStay {
-    //UC1
+    //UC2
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Book My Stay v1.0 ");
-        System.out.println(" Hotel Booking Management System ");
-        System.out.println("=================================");
+        System.out.println("===== Available Room Details =====");
 
-        System.out.println("Application Started Successfully.");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
+
+
+abstract class Room {
+
+    protected String type;
+    protected double price;
+
+    public Room(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Price: ₹" + price);
+    }
+}
+
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 6000);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-/TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
