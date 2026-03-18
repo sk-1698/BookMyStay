838e756ebf0093680c31f60a31d7cf3661244a66
sk-1698 <sk1698@srmist.edu.in>
2026-03-18 14:53:21 +0530

    UC1 - Application entry and welcome message

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,12 @@
 public class BookMyStay {
+    //UC1
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay v1.0 ");
+        System.out.println(" Hotel Booking Management System ");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully.");
+    }
 }
